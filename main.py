d6fd4aa79c15ef2abfbc141e3166eca25b21a31d
Arturo Regueira <arturomcgillicutty@outlook.com>
2025-05-06 22:13:03 -0400

    plus laboris feci

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 # main.py
 
-from edgar_api import get_company_submissions, save_JSON
+import  edgar_api #functiones receptae ex edgar_api.py
 
+
+def main():
+    ticker = input("Praebe ticker").upper()
+    form_type = input("Praebe forma").upper()
+    count = int(input("quantitas"))
+    
+    print(f"Recuperatio inscriptionum pro {ticker}...")
+    data = edgar_api.get_company_submissions(ticker)
+    edgar_api.save_JSON(data,f"{ticker}--filings.json")
+
+    print(f" Filtratio usque ad {count} '{form_type}' formae...")
+
+    try:
+        matches = edgar_api.get_filings(data,form_type=form_type,count=count )
+        for m in matches:
+            print(f"� {m['filingDate']} - {form_type} - {m['url']}")
+    except ValueError as e:
+        print("reiectus quia",e)
 
 if __name__ == "__main__":
-    ticker = "AAPL"  # Apple Inc.
-    data = get_company_submissions(ticker)
-    
-    save_JSON(data,f"{ticker}-filings.json")
+    main()
--- test.py
+++
@@ -1,5 +0,0 @@
-from edgar_api import get_company_submissions
-
-
-ticker = "AAPL"  # Apple Inc.
-get_company_submissions(ticker)
